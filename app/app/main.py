040d51eb93a240e90248b14bb137c5e4f449b7c1
mateusz.kluswoski <mateusz.klusowski@slickcloud.io>
2023-06-09 15:39:41 +0200

    feature: I realized that it will be better way to do front in react or something similar

--- app/app/main.py
+++ app/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+
 from routes import links, users
 
 app = FastAPI()
+
 # Routes
 app.include_router(links.router)
 app.include_router(users.router)
 
 
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello World"}
