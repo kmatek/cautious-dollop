7df7a95d0a7124460507b7102d1249e33d831f6a
mateusz.kluswoski <mateusz.klusowski@slickcloud.io>
2023-06-13 21:44:12 +0200

    feature: add login html

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={``}>{children}</body>
+      <body className="bg-[#1e293b]">{children}</body>
     </html>
   )
 }
