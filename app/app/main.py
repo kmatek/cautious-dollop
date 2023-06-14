2ac4690d6d49ddb8a232a1deaab554be238edab4
mateusz.kluswoski <mateusz.klusowski@slickcloud.io>
2023-06-14 17:01:28 +0200

    feature: add cors handling

--- app/app/main.py
+++ app/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import settings
 from routes import links, users
 
+
 app = FastAPI()
+
 # Routes
 app.include_router(links.router)
 app.include_router(users.router)
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.get_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
